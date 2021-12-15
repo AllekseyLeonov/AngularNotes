dad5e7034191c75b60162c3c5ffcddc63e7b6068
Aleksey Leonov <leonovalleksey@gmail.com>
2021-12-15 17:37:02 +0300

    Install MatUi. Create Header and Note components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 import { AppComponent } from './app.component';
+import { NoteComponent } from './components/note/note.component';
+import { HeaderComponent } from './components/header/header.component';
+
+// TODO: remove 'notes' page to a separate module
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/note/note.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-note',
+  templateUrl: './note.component.html',
+  styleUrls: ['./note.component.css']
+})
+export class NoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  @Input() title = "Title"
+  @Input() description = "Description"
+}
